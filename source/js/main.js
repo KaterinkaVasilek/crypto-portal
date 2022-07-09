0106fa3d25b0368d377bc3393ce3476a03360ae3
KaterinkaVasilek <e.kushner@bk.ru>
2022-07-09 03:47:47 +0300

    feat: project completed

---
+++ source/js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let toggleButton = document.querySelector(".toggle__menu"), 
+navBar = document.querySelector(".nav__bar"); 
+
+toggleButton.addEventListener("click", (function () { navBar.classList.toggle("toggle") }));
+
+let burgerBtn = document.querySelector('#btn');
+let list = document.querySelector('#nav__list');
+burgerBtn.addEventListener('click', function(){
+list.classList.toggle('active');
+})
+
+function trackScroll() {
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        arrowUp.classList.add('up__btn');
+    }
+    if (scrolled < coords) {
+        arrowUp.classList.remove('up__btn');
+    }
+}
+
+function backToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+}
+
+let arrowUp = document.querySelector('.up');
+
+window.addEventListener('scroll', trackScroll);
+arrowUp.addEventListener('click', backToTop);
